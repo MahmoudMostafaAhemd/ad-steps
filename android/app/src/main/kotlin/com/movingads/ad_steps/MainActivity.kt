138d867836e65a301ed969bb212d4e2a288d0ce9
mahmoud <dev.mahmoud.mostafa@gmail.com>
2019-06-21 14:04:28 +0200

    init msg

---
+++ android/app/src/main/kotlin/com/movingads/ad_steps/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.movingads.ad_steps
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
